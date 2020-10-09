5379fef1026a8905a891f6d2adaa31ec63353760
com.Kondratov <com.Kondratov@gmail.com>
2020-10-09 19:51:03 +0300

    Create Startup constructor and connect appsettingconfig.json to ConfigureServices

--- Company_WebSite/Startup.cs
+++ Company_WebSite/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Company_WebSite.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,13 +15,17 @@ namespace Company_WebSite
 {
     public class Startup
     {
-       
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
         public void ConfigureServices(IServiceCollection services)
         {
+            // Подключаю конфик из appsettingconfig.json
+            Configuration.Bind("Project", new Config());
             // Добовляем поддержку контроллеров и представлений (MVC)
             services.AddControllersWithViews()
             // Выстовляем совместимость с ASP.net CORE 3.0
             .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
+
 
 
         }

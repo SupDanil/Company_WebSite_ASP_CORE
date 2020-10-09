614c2a6fa728e9ca15c2c2132c258e99d18e2ae3
com.Kondratov <com.Kondratov@gmail.com>
2020-10-09 19:44:39 +0300

    Create Config class in Service folder

---
+++ Company_WebSite/Service/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company_WebSite.Service
+{
+    public class Config
+    {
+      public static string  CompanyName {get;set;}
+      public static string  CompanyPhone {get;set;}
+      public static string  CompanyPhoneShort {get;set;}
+      public static string CompanyEmail {get;set; }
+    }
+}

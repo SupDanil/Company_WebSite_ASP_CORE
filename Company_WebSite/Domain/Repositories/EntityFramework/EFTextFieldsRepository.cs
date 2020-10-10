74900cbd126701fa28fea2e36da9e757f878cfde
com.Kondratov <com.Kondratov@gmail.com>
2020-10-10 11:07:07 +0300

    Realize  ITextFieldsRepository interface

--- Company_WebSite/Domain/Repositories/EntityFramework/EFTextFieldsRepository.cs
+++ Company_WebSite/Domain/Repositories/EntityFramework/EFTextFieldsRepository.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿using Company_WebSite.Domain.Entities;
+using Company_WebSite.Domain.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace Company_WebSite.Domain.Repositories.EntityFramework
 {
-    public class EFTextFieldsRepository
+    public class EFTextFieldsRepository : ITextFieldsRepository
     {
+        private readonly AppDbContext context;
+        public EFTextFieldsRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public void DeleteTextField(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TextField GetTextFieldByCodeWord(string CodeWord)
+        {
+            return context.TextFields.FirstOrDefault(x => x.CodeWord == CodeWord);
+        }
+
+        public TextField GetTextFieldById(Guid Id)
+        {
+            return context.TextFields.FirstOrDefault(x => x.Id == Id);
+        }
+
+        public IQueryable<TextField> GetTextFields()
+        {
+            return context.TextFields;
+        }
+
+        public void SaveTextFields(TextField entity)
+        {
+            if (entity.Id == default)
+                context.Entry(entity).State = EntityState.Added;
+            else
+                context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
     }
 }

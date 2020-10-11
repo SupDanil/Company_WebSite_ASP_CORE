1b4136db74bfb8cfa766f278a651222e6aa0905f
com.Kondratov <com.Kondratov@gmail.com>
2020-10-11 18:05:46 +0300

    Fix some bugs

--- Company_WebSite/Areas/Admin/Controllers/HomeController.cs
+++ Company_WebSite/Areas/Admin/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Company_WebSite.Domain;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company_WebSite.Areas.Admin.Controllers
@@ -9,9 +10,15 @@ namespace Company_WebSite.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+            
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.ServiceItems.GetServiceItem());
         }
     }
 }

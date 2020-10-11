ed2fae998b1015f3a6005a9dabc8b27f48451f48
com.Kondratov <com.Kondratov@gmail.com>
2020-10-11 11:43:22 +0300

    Make migration with PowerShell

--- Company_WebSite/Domain/AppDbContext.cs
+++ Company_WebSite/Domain/AppDbContext.cs
@@ -22,14 +22,14 @@ namespace Company_WebSite.Domain
 
             modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
             {
-                Id = "iUNFjWQ5-ctP0-J3bH-uFi6-o3ihaf8tJNU7",
+                Id = "58356934-6184-0354-7295-5478392143", //"iUNFjWQ5-ctP0-J3bH-uFi6-o3ihaf8tJNU7",
                 Name = "admin",
                 NormalizedName = "ADMIN"
             });
 
             modelBuilder.Entity<IdentityUser>().HasData(new IdentityUser
             {
-                Id = "oYV9cgjS-cPbA-hKvI-oe4v-M165pjc6i7xz",
+                Id = "43811234-7623-8643-9462-0192837465", //"oYV9cgjS-cPbA-hKvI-oe4v-M165pjc6i7xz",
                 UserName = "admin",
                 NormalizedUserName = "ADMIN",
                 Email = "fer@gmail.com",
@@ -40,24 +40,24 @@ namespace Company_WebSite.Domain
             });
             modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
             {
-              RoleId = "lsgC9zoH-RcK4-dNmQ-B515-z1PosqtUvK",
-              UserId = "MIvX0Aoc-1zNZ-oSEZ-JH9w-eV0vNnQj6i"
+              RoleId = "69345218-9904-1256-6732-4362765413",   //"lsgC9zoH-RcK4-dNmQ-B515-z1PosqtUvK",
+              UserId = "41329012-6473-8234-0912-5566443728"   //"MIvX0Aoc-1zNZ-oSEZ-JH9w-eV0vNnQj6i"
             });
             modelBuilder.Entity<TextField>().HasData(new TextField
             {
-                Id = new Guid("pVJNUVnD-ztci-8M0o-gFsj-fwPgijZku8"),
+                Id = Guid.NewGuid(),    //"pVJNUVnD-ztci-8M0o-gFsj-fwPgijZku8"
                 CodeWord = "PageIndex",
                 Title = "Главная"
             });
             modelBuilder.Entity<TextField>().HasData(new TextField
             {
-                Id = new Guid("wnHIk8jg-pGns-puIK-waeU-VerNyfF02u"),
+                Id = Guid.NewGuid(),    //"wnHIk8jg-pGns-puIK-waeU-VerNyfF02u"
                 CodeWord = "PageServices",
                 Title = "Наши услуги"                                                
             });
             modelBuilder.Entity<TextField>().HasData(new TextField
             {
-                Id = new Guid("PFKka935-3Hze-ce8G-PZNg-lXieLFGHqO"),
+                Id = Guid.NewGuid(),                 //"PFKka935-3Hze-ce8G-PZNg-lXieLFGHqO"
                 CodeWord = "PageContacts",
                 Title = "Контакты"
             });
---
+++ Company_WebSite/Migrations/20201011084223__initial.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Company_WebSite.Migrations
+{
+    public partial class _initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUsers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(maxLength: 256, nullable: true),
+                    Email = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    LockoutEnabled = table.Column<bool>(nullable: false),
+                    AccessFailedCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    TitleImagePath = table.Column<string>(nullable: true),
+                    MetaTitle = table.Column<string>(nullable: true),
+                    MetaDiscription = table.Column<string>(nullable: true),
+                    MetaKeywords = table.Column<string>(nullable: true),
+                    DataAdded = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Subtitle = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TextFields",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Subtitle = table.Column<string>(nullable: true),
+                    TitleImagePath = table.Column<string>(nullable: true),
+                    MetaTitle = table.Column<string>(nullable: true),
+                    MetaDiscription = table.Column<string>(nullable: true),
+                    MetaKeywords = table.Column<string>(nullable: true),
+                    DataAdded = table.Column<DateTime>(nullable: false),
+                    CodeWord = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TextFields", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoleClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<string>(nullable: false),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: false),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(nullable: false),
+                    ProviderKey = table.Column<string>(nullable: false),
+                    ProviderDisplayName = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    RoleId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    LoginProvider = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "58356934-6184-0354-7295-5478392143", "0a5545aa-ca06-4cd2-8933-794fac9b0edc", "admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "UserId", "RoleId" },
+                values: new object[] { "41329012-6473-8234-0912-5566443728", "69345218-9904-1256-6732-4362765413" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "43811234-7623-8643-9462-0192837465", 0, "db7093d5-3e38-4785-b23a-bed9f9b8b27b", "fer@gmail.com", true, false, null, "FER@GMAIL.COM", "ADMIN", "AQAAAAEAACcQAAAAEKIuAQSno8/EOdgeUtRbdt9awfaB0Q3ikAmLtuxauuKvUS2D022qaYgR766XwpQhTw==", null, false, "", false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "TextFields",
+                columns: new[] { "Id", "CodeWord", "DataAdded", "MetaDiscription", "MetaKeywords", "MetaTitle", "Subtitle", "Text", "Title", "TitleImagePath" },
+                values: new object[,]
+                {
+                    { new Guid("b95d11b2-af99-41ac-9204-c32b25b0a91c"), "PageIndex", new DateTime(2020, 10, 11, 8, 42, 23, 62, DateTimeKind.Utc).AddTicks(1089), null, null, null, null, "Содержание заполняется администратором", "Главная", null },
+                    { new Guid("07c3c90a-e380-4997-87a7-0cfccdb6a07e"), "PageServices", new DateTime(2020, 10, 11, 8, 42, 23, 62, DateTimeKind.Utc).AddTicks(4782), null, null, null, null, "Содержание заполняется администратором", "Наши услуги", null },
+                    { new Guid("4d5977e0-c72e-4b30-be7d-ec7a876e7514"), "PageContacts", new DateTime(2020, 10, 11, 8, 42, 23, 62, DateTimeKind.Utc).AddTicks(4905), null, null, null, null, "Содержание заполняется администратором", "Контакты", null }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "ServiceItems");
+
+            migrationBuilder.DropTable(
+                name: "TextFields");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+        }
+    }
+}

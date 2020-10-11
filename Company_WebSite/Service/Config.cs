e183eda17cc197e8efb69d33e145257d3a9da7ad
com.Kondratov <com.Kondratov@gmail.com>
2020-10-11 10:45:27 +0300

    Add new Services to ConfigureServices

--- Company_WebSite/Service/Config.cs
+++ Company_WebSite/Service/Config.cs
@@ -7,6 +7,7 @@ namespace Company_WebSite.Service
 {
     public class Config
     {
+      public static string ConnectionString { get; set; }
       public static string  CompanyName {get;set;}
       public static string  CompanyPhone {get;set;}
       public static string  CompanyPhoneShort {get;set;}
--- Company_WebSite/Startup.cs
+++ Company_WebSite/Startup.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Company_WebSite.Domain;
+using Company_WebSite.Domain.Repositories.Abstract;
+using Company_WebSite.Domain.Repositories.EntityFramework;
 using Company_WebSite.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,13 +26,41 @@ namespace Company_WebSite
         {
             // Подключаю конфик из appsettingconfig.json
             Configuration.Bind("Project", new Config());
+
+            // Подключаем нужный функционал приложения в качестве сервисов
+            services.AddTransient<ITextFieldsRepository, EFTextFieldsRepository>();
+            services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+            services.AddTransient<DataManager>();
+
+            // Подключаем контекст к БД
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            // Настраиваем Identity систему
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            // Настраиваем Authentication cookie
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "myCompanyAuth";
+                options.Cookie.HttpOnly = true;
+                options.LoginPath = "/account/login";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.SlidingExpiration = true;
+            });       
+
             // Добовляем поддержку контроллеров и представлений (MVC)
             services.AddControllersWithViews()
+
             // Выстовляем совместимость с ASP.net CORE 3.0
             .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
-
-
-
         }
 
         

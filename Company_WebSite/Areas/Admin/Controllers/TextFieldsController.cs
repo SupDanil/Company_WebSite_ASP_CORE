b5a8c993407c4e3412478f9b96b08e497cca6444
com.Kondratov <com.Kondratov@gmail.com>
2020-10-12 08:50:49 +0300

    Create TextFiels admin controller

---
+++ Company_WebSite/Areas/Admin/Controllers/TextFieldsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Company_WebSite.Controllers;
+using Company_WebSite.Domain;
+using Company_WebSite.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company_WebSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TextFieldsController : Controller
+    {
+        private readonly DataManager dataManager;
+        public TextFieldsController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Edit(string codeWord)
+        {
+            var entity = dataManager.TextFields.GetTextFieldByCodeWord(codeWord);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TextField model)
+        {
+            if (ModelState.IsValid)
+            {
+                dataManager.TextFields.SaveTextFields(model);
+                return RedirectToAction(nameof(HomeController.Index));  /*, nameof(HomeController).CutController()*/
+            }
+            return View(model);
+        }
+
+    }
+}

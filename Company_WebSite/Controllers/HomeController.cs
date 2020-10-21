4315b9c8eb9618f4ecf5aa21b34f3286260919b5
com.Kondratov <com.Kondratov@gmail.com>
2020-10-21 18:37:11 +0300

    Create Index and Contacts Action Metoths

--- Company_WebSite/Controllers/HomeController.cs
+++ Company_WebSite/Controllers/HomeController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Company_WebSite.Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company_WebSite.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager; 
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }

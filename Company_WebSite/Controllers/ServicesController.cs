d7332cb4fbad2c523f47c8cb5ffbb50604a2c6a1
com.Kondratov <com.Kondratov@gmail.com>
2020-10-24 17:12:20 +0300

    Create Services Controller

---
+++ Company_WebSite/Controllers/ServicesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Company_WebSite.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company_WebSite.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly DataManager dataManager;
+        public ServicesController (DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+        public IActionResult Index(Guid id)
+        {
+            if (id != default)
+            {
+                return View("Show", dataManager.ServiceItems.GetErviceItemById(id));
+            }
+            ViewBag.TextField = dataManager.TextFields.GetTextFieldByCodeWord("PageServices");
+            return View(dataManager.ServiceItems.GetServiceItem());
+        }
+
+    }
+}
